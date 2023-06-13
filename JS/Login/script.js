852b3bc0b3b7da6d5dee2ae164e0c252db000a77
matheus <matheussfilipej@gmail.com>
2023-06-13 12:27:02 -0300

    challenger

---
+++ JS/Login/script.js
@@ -0,0 +1,8 @@
+function contar() {
+    var ini = document.getElementById ('txti')
+    var fim = document.getElementById ('txtf')
+    var Passo  = document.getElementById ('txtp')
+
+    if (ini.value.lenght == 0 ││ Fim.value.lenght == 0 Passo.value.lenght ==0)
+
+}
